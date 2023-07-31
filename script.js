4fb127af182c22c9604f2ba7392ac7511af88c9d
Ajgorek <igorblendzinski12@gmail.com>
2023-07-31 23:02:12 +0200

    Add script for Categories on click In future this will give dispal none for parent element

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ const popUpBox = document.querySelector(".popUpBox");
 document.addEventListener("DOMContentLoaded", () => {
   popUpBox.style.transform = "translateY(0)";
 });
+
+const categoriesLI = document.querySelectorAll(".categories li");
+
+categoriesLI.forEach((li) => {
+  li.addEventListener("click", () => {
+    console.log("chuj");
+  });
+});
